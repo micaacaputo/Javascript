a0244b58177744d62ab427c989a0f40029a6ecfc
Mica Caputo <49381405+micaacaputo@users.noreply.github.com>
2024-10-09 01:33:48 -0300

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,16 @@
+//Variables al azar
+//let nombre = Jesica;
+//let edad = 26;
+//let profesion = psicologa;
+
+
+let nombre = prompt("Ingresa tu nombre:");
+let edad = prompt("Ingresa tu edad:");
+let profesion = prompt("Ingresa tu profesión:");
+let dias = edad * 365;
+
+let mensaje = "¡Hola " + nombre + "! Tenés " + edad + " y tu profesión es " + profesion + "\n";
+let mensaje2 = "Viviste " + dias + " días desde que naciste";
+
+alert(mensaje);
+alert(mensaje2);
